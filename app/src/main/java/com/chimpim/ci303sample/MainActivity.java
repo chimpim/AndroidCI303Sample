fe1e096bc14340d0c2323ff44bda017110b2bf10
TuuZed <liuyonghui.job@gmail.com>
2018-01-23 17:37:41 +0800

    升级ci303版本为v1.2.2

--- app/src/main/java/com/chimpim/ci303sample/MainActivity.java
+++ app/src/main/java/com/chimpim/ci303sample/MainActivity.java
@@ -2,17 +2,18 @@ package com.chimpim.ci303sample;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.text.TextUtils;
+import android.os.SystemClock;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.chimpim.reader.ci303.CI303Reader;
 import com.chimpim.reader.ci303.CI303ReaderSupport;
-import com.chimpim.reader.ci303.CI303Reader_AndroidSerialPortImpl;
 import com.chimpim.reader.ci303.TagData;
+import com.chimpim.serialport.AndroidSerialPort;
+import com.chimpim.serialport.SerialPortException;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -23,6 +24,7 @@ public class MainActivity extends Activity {
     private Button mBtnStartRead;
     private Button mBtnStopRead;
     private TextView mTvLog;
+    private EditText mEtSerialPort;
     private CI303Reader mCI303Reader;
 
     private AtomicBoolean isStartRead = new AtomicBoolean(false);
@@ -35,28 +37,30 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
-        mCI303Reader = new CI303Reader_AndroidSerialPortImpl();
-        mBtnConnect.setOnClickListener(v -> runOnIoThread(() -> {
-            // 自动检测串口号
-            String connect = CI303ReaderSupport.connect(mCI303Reader);
-            runOnUiThread(() -> {
-                if (TextUtils.isEmpty(connect)) {
-                    Toast.makeText(this, "检测不到CI303读卡器", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(this, "连接成功(" + connect + ")", Toast.LENGTH_SHORT).show();
+        final String port = mEtSerialPort.getText().toString();
+        mCI303Reader = new CI303Reader(new AndroidSerialPort(), port, 9600);
+        mBtnConnect.setOnClickListener(v -> {
+            //
+            runOnIoThread(() -> {
+                try {
+                    mCI303Reader.connect();
+                    runOnUiThread(() -> Toast.makeText(this, "连接成功(" + port + ")", Toast.LENGTH_SHORT).show());
+                } catch (SerialPortException e) {
+                    e.printStackTrace();
+                    runOnUiThread(() -> Toast.makeText(this, "连接失败(" + e + ")", Toast.LENGTH_SHORT).show());
                 }
             });
-
-        }));
+        });
         mBtnStartRead.setOnClickListener(v -> runOnIoThread(() -> {
             isStartRead.set(true);
             while (isStartRead.get()) {
                 try {
                     TagData[] tagData = CI303ReaderSupport.gen2MultiTagIdentify(mCI303Reader, 1000);
                     runOnUiThread(() -> mTvLog.setText(Arrays.toString(tagData) + "\n" + mTvLog.getText()));
-                } catch (IOException e) {
+                } catch (Exception e) {
                     e.printStackTrace();
                     runOnUiThread(() -> mTvLog.setText("读卡异常:" + e.toString() + "\n" + mTvLog.getText()));
+                    SystemClock.sleep(1000);
                 }
             }
         }));
@@ -64,10 +68,11 @@ public class MainActivity extends Activity {
     }
 
     private void initView() {
-        mBtnConnect = (Button) findViewById(R.id.btn_connect);
-        mBtnStartRead = (Button) findViewById(R.id.btn_start_read);
-        mBtnStopRead = (Button) findViewById(R.id.btn_stop_read);
-        mTvLog = (TextView) findViewById(R.id.tv_log);
+        mBtnConnect = findViewById(R.id.btn_connect);
+        mBtnStartRead = findViewById(R.id.btn_start_read);
+        mBtnStopRead = findViewById(R.id.btn_stop_read);
+        mTvLog = findViewById(R.id.tv_log);
+        mEtSerialPort = findViewById(R.id.et_serial_port);
     }
 
     private void runOnIoThread(Runnable action) {
